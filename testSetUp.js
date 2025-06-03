c95e1b61797baff80c0325c6f05f0d9ecd210fca
Topler97 <yhjs23.toplam@folkuniversitetet.nu>
2025-06-03 18:08:09 +0200

    try again

--- testSetUp.js
+++ testSetUp.js
@@ -1,7 +1,7 @@
 import { afterEach, beforeAll, afterAll } from "vitest";
 import { cleanup } from "@testing-library/react";
 import "@testing-library/jest-dom/vitest";
-import { server } from "./src/mocks/server";
+import { server } from "./src/mockData/server";
 import "@testing-library/jest-dom";
 
 beforeAll(() => server.listen());
